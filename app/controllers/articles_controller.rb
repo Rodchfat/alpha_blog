5e09c20bb3168561b72f853b396e7b2e694df1ea
Rails Course <admin@pmplinks.com>
2016-01-20 19:51:40 +0000

    complete new and show action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,12 +5,23 @@ class ArticlesController < ApplicationController
     
     def create
         @article = Article.new(article_params)
-        @article.save
-        redirect_to articles_show(@article)
+        if @article.save
+        flash[:notice] = "Article was successfuly created"
+        redirect_to articles_path(@article)
+        else render 'new'
+        end
     end
+    
+    
+    def show
+    @article = Article.find(params[:id])
+    end
+    
+    
    private
     def article_params
     params.require(:article).permit(:title, :description)
     end
+    
     
 end
