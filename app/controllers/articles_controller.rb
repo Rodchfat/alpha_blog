8e0adfcdc90f078ff320d0c9c3df5e3bdbb6c1ff
Rails Course <admin@pmplinks.com>
2016-01-21 05:04:57 +0000

    Deploy and Wrap Up

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
     
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
     def index
         @articles = Article.all
     end
@@ -19,11 +20,11 @@ class ArticlesController < ApplicationController
     end
     
     def edit
-        @article = Article.find(params[:id])
+        
     end
     
     def update
-        @article = Article.find(params[:id])
+        
         if @article.update(article_params)
             flash[:notice] = "Article was successfully update"
             redirect_to article_path(@article)
@@ -34,7 +35,7 @@ class ArticlesController < ApplicationController
     end
         
         def destroy
-                @article = Article.find(params[:id])
+                
                 @article.destroy
                 flash[:notice] = "Article was successfully deleted"
                 redirect_to articles_path
@@ -50,6 +51,13 @@ class ArticlesController < ApplicationController
     
     
    private
+   
+    def set_article
+        @article = Article.find(params[:id])
+    end
+    
+        
+       
     def article_params
         params.require(:article).permit(:title, :description)
     end
